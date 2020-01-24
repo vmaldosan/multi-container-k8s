a7e6f8b35d68bb72f25ebdd24bc10033e21c3c5e
Maldonado, Victor <victor.maldonado@optum.com>
2020-01-24 18:09:28 +0000

    Fixing error when deploying in Travis.

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -1,9 +1,11 @@
 docker build -t vmaldosan/multi-client:latest -t vmaldosan/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t vmaldosan/multi-server:latest -t vmaldosan/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t vmaldosan/multi-worker:latest -t vmaldosan/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
 docker push vmaldosan/multi-client:latest
 docker push vmaldosan/multi-server:latest
 docker push vmaldosan/multi-worker:latest
+
 docker push vmaldosan/multi-client:$GIT_SHA
 docker push vmaldosan/multi-server:$GIT_SHA
 docker push vmaldosan/multi-worker:$GIT_SHA
