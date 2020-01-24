167540c0b911dd47a4f25a7ce5999735ee5b2b42
Maldonado, Victor <victor.maldonado@optum.com>
2020-01-24 17:39:40 +0000

    Adds config for Google Cloud in travis file. Adds deploy script.

---
+++ k8s/deploy.sh
@@ -0,0 +1,14 @@
+docker build -t vmaldosan/multi-client:latest -t vmaldosan/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t vmaldosan/multi-server:latest -t vmaldosan/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t vmaldosan/multi-worker:latest -t vmaldosan/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker push vmaldosan/multi-client:latest
+docker push vmaldosan/multi-server:latest
+docker push vmaldosan/multi-worker:latest
+docker push vmaldosan/multi-client:$GIT_SHA
+docker push vmaldosan/multi-server:$GIT_SHA
+docker push vmaldosan/multi-worker:$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=vmaldosan/multi-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=vmaldosan/multi-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=vmaldosan/multi-worker:$GIT_SHA
